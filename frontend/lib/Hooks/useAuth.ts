4334ccd0ec891e69fca6a42b87fd72d722e217a2
Masomeh Rezaei <masomehrezaei83.r@gmail.com>
2025-07-26 19:30:56 +0330

    update front

--- frontend/lib/Hooks/useAuth.ts
+++ frontend/lib/Hooks/useAuth.ts
@@ -1,7 +1,6 @@
 import { useMutation } from '@tanstack/react-query'
 import { registerUser ,loginUser } from '@/lib/api/auth'
-import {useRouter} from "next/navigation";
-import {api} from "@/lib/api";
+
 type RegisterData = {
     name: string // 👈 اضافه کردن فیلد نام
     email: string
@@ -14,26 +13,13 @@ export const useRegister = () => {
     })
 }
 
-type LoginPayload = {
+type LoginData = {
     email: string
     password: string
 }
 
 export const useLogin = () => {
-    const router = useRouter()
-
-    return useMutation({
-        mutationFn: (data: LoginPayload) => api({
-            url: '/api/auth/login',
-            method: 'POST',
-            data,
-        }),
-        onSuccess: (response) => {
-            localStorage.setItem('access_token', response.access_token)
-            localStorage.setItem('refresh_token', response.refresh_token)
-            router.push('/form') // یا هر صفحه‌ای که بعد از لاگین می‌ری
-            console.log('reftesh',response.refresh_token)
-            console.log('access',response.access_token)
-        },
+    return useMutation<unknown, Error, LoginData>({
+        mutationFn: loginUser,
     })
 }
--- frontend/lib/api/auth.ts
+++ frontend/lib/api/auth.ts
@@ -1,24 +1,12 @@
 import axiosClient from '@/lib/axiosClient'
 
-type AuthPayload = {
+type LoginData = {
     email: string
     password: string
 }
 
-export const loginUser = async (data: AuthPayload) => {
+export const loginUser = async (data: LoginData) => {
     const res = await axiosClient.post('/api/auth/login', data)
-
-
-    localStorage.setItem('access_token', res.data.access_token)
-    localStorage.setItem('refresh_token', res.data.refresh_token)
-<<<<<<< HEAD
-    console.log('refresh tocken',res.data.refresh_token)
-    console.log('access tocken',res.data.access_token)
-=======
-    console.log('refresh',res.data.refresh_token)
-    console.log('access',res.data.access_token)
->>>>>>> 75b345a2782f41af246226ae41e08128ee864c62
-
     return res.data
 }
 
